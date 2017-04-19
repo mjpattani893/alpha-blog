970d0de31491dcb661447619fbce5f431555fd73
Maitreyi <maitreyipattani@gmail.com>
2017-04-19 10:14:23 -0700

    create destroy method. Add partials.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
 	def index
 		@articles = Article.all
 	end
-	
+
 	def new
 		@article = Article.new
 	end
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
 		# @article.save
 		# redirect_to article_path(@article)
 		if @article.save
-			flash[:notice] = "Article was created succesfully."
+			flash[:notice] = "Article was created successfully."
 			redirect_to article_path(@article)
 		else
 			render 'new'
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
 	def update
 		@article = Article.find(params[:id])
 		if @article.update(article_params)
-			flash[:notice] = "Article was updated succesfully."
+			flash[:notice] = "Article was updated successfully."
 			redirect_to article_path(@article)
 		else
 			render 'edit'
@@ -36,6 +36,13 @@ class ArticlesController < ApplicationController
 
 	def show
 		@article = Article.find(params[:id])
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy 
+		flash[:notice] = "Article was successfully deleted"
+		redirect_to articles_path
 	end
 
 	private
