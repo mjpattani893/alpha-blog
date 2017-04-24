c226ff1d1e87ce39e4141b58bfd8d1be41122e58
Maitreyi Pattani <maitreyi.pattani@einfochips.com>
2017-04-24 10:27:47 +0530

    email regex updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 						 uniqueness: {case_sensitivity: false}, 
 						 length: {minimum: 3, maximum: 25}
 
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 	validates :email, presence: true,
 					  length: {maximum: 105},
