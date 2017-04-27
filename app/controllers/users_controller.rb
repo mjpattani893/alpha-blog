56aee267f97194f8b9d4856d0e86306307200b80
Maitreyi <maitreyipattani@gmail.com>
2017-04-26 19:02:24 -0700

    edit action and show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:edit, :show, :update, :destroy]
+
 	def new
 		@user = User.new
+	end
+
+	def edit
+		# @user = User.find(params[:id])
 	end
 
 	def create
@@ -13,7 +19,22 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def update
+		if @user.update(user_params)
+			flash[:success] = "Your account was updated successfully."
+			redirect_to articles_path
+		else
+			render 'edit'
+		end
+	end
+
+	def show
+	end
+
 	private 
+		def set_user
+			@user = User.find(params[:id])
+		end
 		def user_params
 			params.require(:user).permit(:username,:email,:password)
 		end
