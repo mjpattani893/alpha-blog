af68c65c694c5852c670a691d200261c578d3a5c
Maitreyi <maitreyipattani@gmail.com>
2017-05-06 22:43:47 -0700

    authenticate all user and article actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 	before_action :set_article, only: [:edit, :show, :update, :destroy]
+	before_action :require_user, except: [:index, :show]
+	before_action :require_same_user, only: [:edit, :update, :destroy]
 
 	def index
 		@articles = Article.paginate(page: params[:page], per_page: 5)
@@ -28,6 +30,8 @@ class ArticlesController < ApplicationController
 
 	def update
 		# @article = Article.find(params[:id])
+		@article.user = current_user
+
 		if @article.update(article_params)
 			flash[:success] = "Article was updated successfully."
 			redirect_to article_path(@article)
@@ -42,15 +46,24 @@ class ArticlesController < ApplicationController
 
 	def destroy
 		@article.destroy 
-		flash[:danger] = "Article was successfully deleted"
+		flash[:danger] = "Article was successfully deleted."
 		redirect_to articles_path
 	end
 
 	private
+
 		def set_article
 			@article = Article.find(params[:id])
 		end
+
 		def article_params
 			params.require(:article).permit(:title,:description)
+		end
+		
+		def require_same_user
+			if current_user != @article.user
+				flash[:danger] = "You can only edit or delete your own articles."
+				redirect_to root_path
+			end
 		end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 	before_action :set_user, only: [:edit, :show, :update, :destroy]
-
+	before_action :require_same_user, only: [:edit, :update]
 	def new
 		@user = User.new
 	end
@@ -37,10 +37,19 @@ class UsersController < ApplicationController
 	end
 
 	private 
+
 		def set_user
 			@user = User.find(params[:id])
 		end
+	
 		def user_params
 			params.require(:user).permit(:username,:email,:password)
+		end
+
+		def require_same_user
+			if current_user != @user
+				flash[:danger] = "You can edit only your own profile."
+				redirect_to root_path
+			end
 		end
 end
