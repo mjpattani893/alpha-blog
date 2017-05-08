4112bc75cd0649c6f38a97761e48fdbb8def3d9d
Maitreyi <maitreyipattani@gmail.com>
2017-05-08 00:16:18 -0700

    Add article category association and UI changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507113401) do
+ActiveRecord::Schema.define(version: 20170508064713) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.index ["article_id"], name: "index_article_categories_on_article_id"
+    t.index ["category_id"], name: "index_article_categories_on_category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
