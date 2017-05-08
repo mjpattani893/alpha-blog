1516a0a2cfe0d91eb2e502d0f9b14dd9fae7a24a
Maitreyi <maitreyipattani@gmail.com>
2017-05-08 02:02:24 -0700

    Edit categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+	before_action :set_category, only: [:edit,:update,:show]
 	before_action :require_admin , except: [:index,:show]
 
 	def index
@@ -19,8 +20,20 @@ class CategoriesController < ApplicationController
 		end
 	end
 
+	def edit
+		# @category = Category.find(params[:id])
+	end
+
+	def update
+		if @category.update(category_params)
+			flash[:success] = "Category name was successfully updated"
+			redirect_to category_path(@category)
+		else
+			render 'edit'
+		end
+	end
+	
 	def show
-		@category = Category.find(params[:id])
 		@category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
 	end
 
@@ -29,6 +42,9 @@ class CategoriesController < ApplicationController
 			params.require(:category).permit(:name)
 		end
 
+		def set_category
+			@category = Category.find(params[:id])
+		end	
 		def require_admin
 			if !logged_in? || (logged_in? && !current_user.admin?)
 				flash[:danger] = "Only admins can perform that action"
